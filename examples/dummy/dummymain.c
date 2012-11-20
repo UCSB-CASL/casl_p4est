26f74274d6db8af0b3eedb44f1bf6c28ecd0e238
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2012-11-20 02:53:45 +0100

    Enhance dummy example

--- examples/dummy/dummymain.c
+++ examples/dummy/dummymain.c
@@ -1,7 +1,15 @@
 
 #include <p4est.h>
 
-int main (int argc, char ** argv)
+static int
+simple_refine (p4est_t * p4est, p4est_topidx_t which_tree,
+        p4est_quadrant_t * quadrant)
+{
+        return which_tree == 0 && quadrant->level < 2;
+}
+
+int
+main (int argc, char ** argv)
 {
 	int mpiret;
 	int mpirank;
@@ -23,13 +31,19 @@ int main (int argc, char ** argv)
 	SC_CHECK_MPI (mpiret);
 
 	/* set log level */
-	lp = SC_LP_ESSENTIAL;
+	/* lp = SC_LP_ESSENTIAL; */
+	lp = SC_LP_DEFAULT;
 	sc_init (mpicomm, 1, 1, NULL, lp);
 	p4est_init (NULL, lp);
 
-	conn = p4est_connectivity_new_brick (1, 1, 0, 0);
+        /* create some p4est refinement */
+	conn = p4est_connectivity_new_brick (1, 2, 0, 0);
 	p4est = p4est_new (mpicomm, conn, 0, NULL, NULL);
+        p4est_refine (p4est, 2, simple_refine, NULL);
+        p4est_partition (p4est, NULL);
+        p4est_vtk_write_file (p4est, NULL, "twobrick");
 
+        /* clean up */
 	p4est_destroy (p4est);
 	p4est_connectivity_destroy (conn);
 
