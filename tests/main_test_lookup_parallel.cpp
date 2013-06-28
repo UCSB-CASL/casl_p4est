092f99602fe611a8aaa5df20cf954bac9b18b3df
Mohammad Mirzadeh <mirzadeh@gmail.com>
2013-06-28 15:37:19 -0700

    Updated the parallel lookup test ... got better but still not correct

--- tests/main_test_lookup_parallel.cpp
+++ tests/main_test_lookup_parallel.cpp
@@ -4,8 +4,6 @@
 #include <p4est_extended.h>
 #include <p4est_vtk.h>
 
-// My files for this project
-
 // System
 #include <stdexcept>
 #include <iostream>
@@ -21,6 +19,8 @@
 #include <src/my_p4est_tools.h>
 #include <src/semi_lagrangian.h>
 #include <src/refine_coarsen.h>
+
+#include <mpi/mpi.h>
 
 // function prototype
 void run_test(p4est_t *p4est, const my_p4est_brick_t *brick);
@@ -43,7 +43,7 @@ int main (int argc, char* argv[]){
   p4est_connectivity_t *connectivity;
   p4est_t              *p4est;
 
-//  srand(time(0));
+  //  srand(time(0));
   rand_grid_data_t data = {9, 1, 5000, 100};
 
   Session::init(argc, argv, mpi->mpicomm);
@@ -104,14 +104,14 @@ int main (int argc, char* argv[]){
 void run_test(p4est_t *p4est, const my_p4est_brick_t* brick)
 {
 
-  {
-      int i = 0;
-      char hostname[256];
-      gethostname(hostname, sizeof(hostname));
-      printf("[%d]: PID %d on %s ready for attach\n", p4est->mpirank, getpid(), hostname);
-      fflush(stdout);
-      sleep(10);
-  }
+  //  {
+  //    int i = 0;
+  //    char hostname[256];
+  //    gethostname(hostname, sizeof(hostname));
+  //    printf("[%d]: PID %d on %s ready for attach\n", p4est->mpirank, getpid(), hostname);
+  //    fflush(stdout);
+  //    sleep(10);
+  //  }
 
   p4est_topidx_t *t2v = p4est->connectivity->tree_to_vertex; // gives corners of each tree
   double         *v2q = p4est->connectivity->vertices;       // physical coordinates of each corner
@@ -193,18 +193,101 @@ void run_test(p4est_t *p4est, const my_p4est_brick_t* brick)
     // find the quadrant
     p4est_quadrant_t qu_match;
     int rank_matched = my_p4est_brick_point_lookup(p4est, ghost, brick, xy, &qu_match, remote_match);
-    P4EST_ASSERT(rank_matched != -1);
 
-    p4est_gloidx_t qu = qu_match.p.piggy3.local_num;
-    tr = qu_match.p.piggy3.which_tree;
-    p4est_tree_t *tree = p4est_tree_array_index(p4est->trees, tr);
-    qu += tree->quadrants_offset + p4est->global_first_quadrant[rank_matched];
+    // We must chech to see if we need to search remote processors
+    // notify receivers that we will be sending them info and figure out who is going to send us stuff!
+    int *recvers = new int[remote_match->elem_count], *senders = new int[p4est->mpisize];
+    int num_recvers = remote_match->elem_count, num_senders;
+
+    for (int i=0; i<num_recvers; i++){
+      p4est_quadrant_t *remote_quad = (p4est_quadrant_t*)sc_array_index(remote_match, i);
+      recvers[i] = remote_quad->p.piggy1.owner_rank;
+    }
+
+    sc_notify(recvers, num_recvers, senders, &num_senders, p4est->mpicomm);
+
+    // Great! now we know who we are sending to (recvers) and receiving from (senders)
+    vector<double> xy_recvd(2*num_senders);      // store xy location of points recvd from other processors
+    vector<int> tr_recvd(num_senders);           // store tr index to be search for recvd points
+    vector<int> qu_gloidx_sendbuf(num_senders);  // send buffer for the quadrants that are found on this processor and should be shipped back
+    vector<int> qu_gloidx_recvbuf(num_recvers);  // recv buffer for the quadrants that are found on other processors and must be recvd
+    vector<int> qu_level_sendbuf (num_senders);  // send buffer to store the level of the quadrant for comparison
+    vector<int> qu_level_recvbuf (num_recvers);  // recv buffer to store the level of the quadrant for comparison
+
+    // send processors the xy location of a point to look for
+    for (int i=0; i<num_recvers; i++){
+      MPI_Send(xy, 2, MPI_DOUBLE, recvers[i], 0, p4est->mpicomm);
+      MPI_Send(&tr, 1, MPI_INT, recvers[i], 1, p4est->mpicomm);
+    }
+
+    // recieve the data from other processors
+    for (int i=0; i<num_senders; i++){
+      MPI_Recv(&xy_recvd[2*i], 2, MPI_DOUBLE, senders[i], 0, p4est->mpicomm, NULL);
+      MPI_Recv(&tr_recvd[i], 1, MPI_INT, senders[i], 1, p4est->mpicomm, NULL);
+    }
+
+    // Now every processor knows what poinst to look up further for others
+    sc_array *remote_dummy = sc_array_new(sizeof(p4est_quadrant_t));
+    for (int i=0; i<num_senders; i++){
+      double xy_tmp [] = {xy_recvd[2*i + 0], xy_recvd[2*i + 1]};
+      p4est_quadrant_t qu;
+      my_p4est_brick_point_lookup(p4est, ghost, brick, xy_tmp, &qu, remote_dummy);
+
+      p4est_tree_t *tree = p4est_tree_array_index(p4est->trees, qu.p.piggy3.which_tree);
+      qu_gloidx_sendbuf[i] = qu.p.piggy3.local_num + tree->quadrants_offset + p4est->global_first_quadrant[p4est->mpirank];
+      qu_level_sendbuf[i]  = qu.level;
+    }
+    sc_array_destroy(remote_dummy);
+
+    // So now every procssor has performed the requests made by the other processor,
+    // they should start sending information back to the owners.
+    for (int i=0; i<num_senders; i++){
+      MPI_Send(&qu_gloidx_sendbuf[i], 1, MPI_INT, senders[i], 0, p4est->mpicomm);
+      MPI_Send(&qu_level_sendbuf[i],  1, MPI_INT, senders[i], 1, p4est->mpicomm);
+    }
+
+    // now recieve data from processors
+    for (int i=0; i<num_recvers; i++){
+      MPI_Recv(&qu_gloidx_recvbuf[i], 1, MPI_INT, recvers[i], 0, p4est->mpicomm, NULL);
+      MPI_Recv(&qu_level_recvbuf[i],  1, MPI_INT, recvers[i], 1, p4est->mpicomm, NULL);
+    }
+
+    // Ok now every processor has asked about all the remote quadrants and has recieved
+    // all the answers it requires. We now need to compare these results against the local
+    // stuff to make a final decision
+    p4est_gloidx_t qu_gloidx;
+    if (rank_matched != -1){
+      qu_gloidx = qu_match.p.piggy3.local_num;
+      tr = qu_match.p.piggy3.which_tree;
+      p4est_tree_t *tree = p4est_tree_array_index(p4est->trees, tr);
+      qu_gloidx += tree->quadrants_offset + p4est->global_first_quadrant[p4est->mpirank];
+    }
+
+    int imin = 0;
+    for (int i=0; i<num_recvers; i++){
+      if (qu_level_recvbuf[i] > qu_level_recvbuf[imin]){
+        imin = i;
+      }
+    }
+
+    if (rank_matched == -1 || (num_recvers != 0 && qu_level_recvbuf[imin] > qu_match.level) ) { //smallest remote quad is smaller than the local one
+      qu_gloidx = qu_gloidx_recvbuf[imin];
+    }
+
+    delete [] recvers;
+    delete [] senders;
+
+    qu_level_sendbuf.clear();
+    qu_level_recvbuf.clear();
+    qu_gloidx_sendbuf.clear();
+    qu_gloidx_recvbuf.clear();
+    xy_recvd.clear();
+    tr_recvd.clear();
 
     list<Cell> &cells = smallest_cells[ni];
     bool pass = false;
-    for (list<Cell>::const_iterator it = cells.begin(); it != cells.end(); ++it)
-    {
-      if (it->idx == qu) {
+    for (list<Cell>::const_iterator it = cells.begin(); it != cells.end(); ++it){
+      if (it->idx == qu_gloidx) {
         pass = true;
         break;
       }
@@ -213,12 +296,11 @@ void run_test(p4est_t *p4est, const my_p4est_brick_t* brick)
     if (!pass){
       ostringstream oss;
       oss << "[ERROR, " << p4est->mpirank << "]: In function '" << __FUNCTION__ << "()', node (" << xy[0] << ", " << xy[1] << ") could belong to any of the following quadrants:\n ";
-      for (list<Cell>::const_iterator it = cells.begin(); it != cells.end(); ++it)
-      {
+      for (list<Cell>::const_iterator it = cells.begin(); it != cells.end(); ++it){
         oss << it->idx << ",";
       }
       oss << endl;
-      oss << "Lookup function returned quadrant " << qu << endl;
+      oss << "Lookup function returned quadrant " << qu_gloidx << endl;
 
       cerr << oss.str() << endl;
 
@@ -235,3 +317,4 @@ void run_test(p4est_t *p4est, const my_p4est_brick_t* brick)
   cout << " Test finished successfully!";
   cout << "\n ***************************\n" << endl;
 }
+
