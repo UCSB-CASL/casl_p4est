bf0be8e67a4f9647c2867725ca0bdeea3bde47f4
Mohammad Mirzadeh <mirzadeh@gmail.com>
2016-02-12 13:18:14 -0500

    vtk compression seems to work fine

--- src/my_p4est_vtk.c
+++ src/my_p4est_vtk.c
@@ -44,7 +44,7 @@
 #include <petsclog.h>
 #include <sys/stat.h>
 
-#undef P4EST_VTK_COMPRESSION
+//#undef P4EST_VTK_COMPRESSION
 #define P4EST_VTK_DOUBLES
 #define P4EST_VTK_BINARY
 
@@ -237,7 +237,7 @@ my_p4est_vtk_write_header (p4est_t * p4est, p4est_nodes_t *nodes, p4est_ghost_t 
     Ntotal = Ncorners;
   }
 
-  snprintf(foldername, BUFSIZ, "%s.vtus", filename);
+  snprintf(foldername, BUFSIZ, "%s.vtu", filename);
   /* create the folder structure on rank = 0 */
   if (mpirank == 0) {
     mkdir(foldername, 0755);
@@ -585,7 +585,7 @@ my_p4est_vtk_write_point_scalar (p4est_t * p4est, p4est_nodes_t *nodes,
   Ntotal = nodes->indep_nodes.elem_count;
 
   /* Have each proc write to its own file */
-  snprintf (vtufilename, BUFSIZ, "%s.vtus/%04d.vtu", filename, mpirank);
+  snprintf (vtufilename, BUFSIZ, "%s.vtu/%04d.vtu", filename, mpirank);
   /* To be able to fseek in a file you cannot open in append mode.
    * so you need to open with "r+" and fseek to SEEK_END.
    */
@@ -735,7 +735,7 @@ my_p4est_vtk_write_cell_scalar (p4est_t * p4est, p4est_ghost_t *ghost,
   FILE               *vtufile;
 
   /* Have each proc write to its own file */
-  snprintf (vtufilename, BUFSIZ, "%s.vtus/%04d.vtu", filename, mpirank);
+  snprintf (vtufilename, BUFSIZ, "%s.vtu/%04d.vtu", filename, mpirank);
   /* To be able to fseek in a file you cannot open in append mode.
    * so you need to open with "r+" and fseek to SEEK_END.
    */
@@ -999,7 +999,7 @@ my_p4est_vtk_write_footer (p4est_t * p4est, const char *filename)
 
 	
   /* Have each proc write to its own file */
-  snprintf (vtufilename, BUFSIZ, "%s.vtus/%04d.vtu", filename, procRank);
+  snprintf (vtufilename, BUFSIZ, "%s.vtu/%04d.vtu", filename, procRank);
   vtufile = fopen (vtufilename, "ab");
   if (vtufile == NULL) {
     P4EST_LERRORF ("Could not open %s for output!\n", vtufilename);
@@ -1066,8 +1066,8 @@ my_p4est_vtk_write_footer (p4est_t * p4est, const char *filename)
     /* Write data about the parallel pieces into both files */
     for (p = 0; p < numProcs; ++p) {
       fprintf (pvtufile,
-               "    <Piece Source=\"%s.vtus/%04d.vtu\"/>\n", vtu_sourcename, p);
-      fprintf (visitfile, "%s.vtus/%04d.vtu\n", filename, p);
+               "    <Piece Source=\"%s.vtu/%04d.vtu\"/>\n", vtu_sourcename, p);
+      fprintf (visitfile, "%s.vtu/%04d.vtu\n", filename, p);
     }
     fprintf (pvtufile, "  </PUnstructuredGrid>\n");
     fprintf (pvtufile, "</VTKFile>\n");
