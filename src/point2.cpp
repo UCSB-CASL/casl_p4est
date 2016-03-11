ad7b952c325c03c641baeb411d1fe7561f985b95
dbochkov <bochkov.ds@gmail.com>
2016-03-11 13:52:28 -0800

    fix area in Point2

--- src/point2.cpp
+++ src/point2.cpp
@@ -143,7 +143,7 @@ double Point2::cross(const Point2& P1, const Point2& P2)
 
 double Point2::area(const Point2& P1, const Point2& P2, const Point2& P3)
 {
-  double sum = (P2.x-P1.x)*(P3.y-P1.y) - (P3.x-P1.x)*(P2.y-P1.y);
+  double sum = ((P2.x-P1.x)*(P3.y-P1.y) - (P3.x-P1.x)*(P2.y-P1.y))*0.5;
   if(sum>0)
     return sum;
   return -sum;
