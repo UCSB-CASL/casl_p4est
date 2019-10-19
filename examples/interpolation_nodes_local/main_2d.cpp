a3d407daebc28a8714dd6280211e33fc888cc51d
bochkov.ds@gmail.com <bochkov.ds@gmail.com>
2019-10-18 18:04:25 -0700

    A somewhat less uglier tools for optimized management of parameters

--- examples/interpolation_nodes_local/main_2d.cpp
+++ examples/interpolation_nodes_local/main_2d.cpp
@@ -31,47 +31,63 @@
 
 #include <src/Parser.h>
 #include <src/casl_math.h>
+#include <src/parameter_list.h>
 
 using namespace std;
 
 const static std::string main_description =
     "In this example, we illustrate interpolation of node-sampled fields using\n"
-    "my_p4est_interpolation_nodes_t class. Specifically, we perform interpolation at points located\n"
-    "displace*(smallest diagonal) away from grid nodes in the direction defined by vector (nx, ny, nz).\n"
-    "One can choose between performing interpolation on the fly (that is, treating the interpolation\n"
-    "object as a CF_2/CF_3 object) or using buffering of the points. The latter metod is always safe\n"
-    "while the former might fail if one of the processes tries to interpolate in the region owned by\n"
-    "another one. Moreover, even if one wants to use on-the-fly interpolation in the local+ghost region\n"
-    "but using a quadratic interpolation, then precomputed second derivatives have to be provided to\n"
-    "ensure success of interpolation. \n"
+    "optimized my_p4est_interpolation_nodes_local_t class. Specifically, we perform interpolation at \n"
+    "the points of a local subgrid of size num_points^d spanning finite volume of a given grid node.\n"
     "Developer: Daniil Bochkov (dbochkov@ucsb.edu), October 2019.\n";
 
+// ---------------------------------------------------------
+// define parameters
+// ---------------------------------------------------------
+param_list_t pl;
+
+// grid parameters
+param_t<int>    lmin       (pl, 10,  "lmin",       "Min level of refinement (default: 4)");
+param_t<int>    lmax       (pl, 10,  "lmin",       "Min level of refinement (default: 4)");
+param_t<double> lip        (pl, 1.5, "lip",        "Lipschitz constant (default: 1.5)");
+param_t<int>    num_splits (pl, 1,   "num_splits", "Number of successive refinements (default: 3)");
+
+// problem set-up (points of iterpolation and function to interpolate)
+param_t<int> num_points    (pl, 5, "num_points"   , "");
+param_t<int> test_function (pl, 0, "test_function", "Test funciton to interpolate (default: 0):\n"
+                                                    "    0 - sin(x)*cos(y)*exp(z)\n"
+                                                    "    1 - ... (to be added)");
+
+// method set-up
+param_t<bool> local (pl, 1, "local", "Perform interpolation \"on-the-fly\" (1) or use buffering of points (0) (default: 1)");
+param_t<int>  order (pl, 2, "order", "Order of interpolation (default: 1): \n"
+                                     "    1 - bi-/tri-linear,\n"
+                                     "    2 - bi-/tri-quadratic,\n"
+                                     "    3 - bi-/tri-quadratic non-oscillatory,\n"
+                                     "    4 - bi-/tri-quadratic non-oscillatory continuous (ver. 1),\n"
+                                     "    5 - bi-/tri-quadratic non-oscillatory continuous (ver. 2)");
 
 // test functions to interpolate
 #ifdef P4_TO_P8
 struct f_cf_t: CF_3 {
-  int test_function;
-  f_cf_t (double test_function) : test_function(test_function) {}
   double operator()(double x, double y, double z) const {
-    switch (test_function)
+    switch (test_function())
     {
       case 0: return sin(x)*cos(y)*exp(z);
       default: throw std::invalid_argument("Invalid test function number");
     }
   }
-};
+} f_cf;
 #else
 struct f_cf_t: CF_2 {
-  int test_function;
-  f_cf_t (double test_function) : test_function(test_function) {}
   double operator()(double x, double y) const {
-    switch (test_function)
+    switch (test_function())
     {
       case 0: return sin(x)*cos(y);
       default: throw std::invalid_argument("Invalid test function number");
     }
   }
-};
+} f_cf;
 #endif
 
 // level-set function for refinement
@@ -97,77 +113,11 @@ int main(int argc, char** argv)
   mpi_environment_t mpi;
   mpi.init(argc, argv);
 
-  // command line parser
+  // get parameter values from command line
   cmdParser cmd;
-
-  // ---------------------------------------------------------
-  // define parameters
-  // ---------------------------------------------------------
-  // grid parameters
-  int    lmin        = 7;   cmd.add_option("lmin"       , "Min level of refinement (default: 4)");
-  int    lmax        = 15;   cmd.add_option("lmax"       , "Max level of refinement (default: 4)");
-  double lip         = 1.5; cmd.add_option("lip"        , "Lipschitz constant (default: 1.5)");
-  int    num_splits  = 1;   cmd.add_option("num_splits" , "Number of successive refinements (default: 3)");
-
-  // problem set-up (points of iterpolation and function to interpolate)
-  double displace      = 0.1; cmd.add_option("displace"     , "Relative (in diagonals) distance between grid nodes and interpolation points (default: 0.1)");
-  double nx            = 1;   cmd.add_option("nx"           , "The x-component of displacement vector");
-  double ny            = 1;   cmd.add_option("ny"           , "The y-component of displacement vector");
-  double nz            = 1;   cmd.add_option("nz"           , "The z-component of displacement vector");
-  int    num_points    = 10;
-  int    test_function = 0;   cmd.add_option("test_function", "Test funciton to interpolate (default: 0):\n"
-                                                              "    0 - sin(x)*cos(y)*exp(z)\n"
-                                                              "    1 - ... (to be added)");
-
-  // method set-up
-  bool precompute_derivatives = 1; cmd.add_option("precompute_derivatives", "Precompute second derivatives (1) or let interpolation function try to compute (0) (default: 1)");
-  int  num_ghost_layers       = 1; cmd.add_option("num_ghost_layers"      , "Number of ghost layers (default: 1)");
-  bool local                  = 0; cmd.add_option("local"            , "Perform interpolation \"on-the-fly\" (1) or use buffering of points (0) (default: 1)");
-  int  order                  = 2; cmd.add_option("order"                 , "Order of interpolation (default: 1): \n"
-                                                                            "    1 - linear,\n"
-                                                                            "    2 - quadratic,\n"
-                                                                            "    3 - quadratic non-oscillatory,\n"
-                                                                            "    4 - quadratic non-oscillatory continuous (ver. 1),\n"
-                                                                            "    5 - quadratic non-oscillatory continuous (ver. 2)");
-
-  // ---------------------------------------------------------
-  // get values from command line
-  // ---------------------------------------------------------
+  pl.initialize_parser(cmd);
   if (cmd.parse(argc, argv, main_description)) return 0;
-
-  lmin        = cmd.get("lmin"      , lmin      );
-  lmax        = cmd.get("lmax"      , lmax      );
-  lip         = cmd.get("lip"       , lip       );
-  num_splits  = cmd.get("num_splits", num_splits);
-
-  test_function = cmd.get("test_function", test_function);
-  displace      = cmd.get("displace"     , displace     );
-  nx            = cmd.get("nx"           , nx           );
-  ny            = cmd.get("ny"           , ny           );
-  nz            = cmd.get("nz"           , nz           );
-
-  precompute_derivatives = cmd.get("precompute_derivatives", precompute_derivatives);
-  num_ghost_layers       = cmd.get("num_ghost_layers"      , num_ghost_layers      );
-  local             = cmd.get("local"            , local            );
-  order                  = cmd.get("order"                 , order                 );
-
-  // ---------------------------------------------------------
-  // some auxiliary preparations
-  // ---------------------------------------------------------
-  // make sure (nx, ny, nz) is a unit vector
-#ifdef P4_TO_P8
-  double norm = sqrt(SQR(nx) + SQR(ny) + SQR(nz));
-  nx /= norm;
-  ny /= norm;
-  nz /= norm;
-#else
-  double norm = sqrt(SQR(nx) + SQR(ny));
-  nx /= norm;
-  ny /= norm;
-#endif
-
-  // create analytical function that will be used for interpolation tests
-  f_cf_t f_cf(test_function);
+  pl.set_from_cmd_all(cmd);
 
   // stopwatch
   parStopWatch w;
@@ -180,7 +130,7 @@ int main(int argc, char** argv)
   // ---------------------------------------------------------
   // loop through all grid resolutions
   // ---------------------------------------------------------
-  for (int iter = 0; iter < num_splits; ++iter)
+  for (int iter = 0; iter < num_splits(); ++iter)
   {
     // ---------------------------------------------------------
     // create grid
@@ -203,7 +153,7 @@ int main(int argc, char** argv)
     p4est = my_p4est_new(mpi.comm(), conn, 0, NULL, NULL);
 
     // refine based on distance to a level-set
-    splitting_criteria_cf_t sp(lmin+iter, lmax+iter, &phi_cf, lip);
+    splitting_criteria_cf_t sp(lmin()+iter, lmax()+iter, &phi_cf, lip());
     p4est->user_pointer = &sp;
     my_p4est_refine(p4est, P4EST_TRUE, refine_levelset_cf, NULL);
 
@@ -211,11 +161,7 @@ int main(int argc, char** argv)
     my_p4est_partition(p4est, P4EST_TRUE, NULL);
 
     // create ghost layer
-    for (int i = 0; i < num_ghost_layers; ++i)
-    {
-      if (i==0) ghost = my_p4est_ghost_new(p4est, P4EST_CONNECT_FULL);
-      else      my_p4est_ghost_expand(p4est, ghost);
-    }
+    ghost = my_p4est_ghost_new(p4est, P4EST_CONNECT_FULL);
 
     // create node structure
     nodes = my_p4est_nodes_new(p4est, ghost);
@@ -230,10 +176,17 @@ int main(int argc, char** argv)
     // ---------------------------------------------------------
     // define variables
     // ---------------------------------------------------------
-    double dxyz[P4EST_DIM]; // dimensions of the smallest quadrants
-    double dxyz_min;        // minimum side length of the smallest quadrants
-    double diag_min;        // diagonal length of the smallest quadrants
+    double dxyz[P4EST_DIM];    // dimensions of the smallest quadrants
+    double dxyz_fv[P4EST_DIM]; // mesh size of the tiny local mesh
+    double dxyz_min;           // minimum side length of the smallest quadrants
+    double diag_min;           // diagonal length of the smallest quadrants
     get_dxyz_min(p4est, dxyz, dxyz_min, diag_min);
+
+    dxyz_fv[0] = dxyz[0]/double(num_points()-1);
+    dxyz_fv[1] = dxyz[1]/double(num_points()-1);
+#ifdef P4_TO_P8
+    dxyz_fv[2] = dxyz[2]/double(num_points()-1);
+#endif
 
     Vec f;        // field to interpolate
     Vec fxx;      // its second derivative in x
@@ -279,7 +232,7 @@ int main(int argc, char** argv)
     // ---------------------------------------------------------
 #ifdef P4_TO_P8
     ngbd.second_derivatives_central(f, fxx, fyy, fzz);
-    switch (order)
+    switch (order())
     {
       case 1:
         interp_g.set_input(f, linear);
@@ -301,7 +254,7 @@ int main(int argc, char** argv)
     }
 #else
     ngbd.second_derivatives_central(f, fxx, fyy);
-    switch (order)
+    switch (order())
     {
       case 1:
         interp_g.set_input(f, linear);
@@ -327,13 +280,17 @@ int main(int argc, char** argv)
     // perform interpolation
     // ---------------------------------------------------------
     double xyz[P4EST_DIM];
-    // get access to f_interp's data
+    double xyz_fv[P4EST_DIM];
+
+    // get access to Vec's data
     ierr = VecGetArray(f_interp, &f_interp_ptr); CHKERRXX(ierr);
+    ierr = VecGetArray(f_exact,  &f_exact_ptr);  CHKERRXX(ierr);
+    ierr = VecGetArray(error,    &error_ptr);    CHKERRXX(ierr);
 
     parStopWatch timing;
     timing.start("Timing interpolation only");
 
-    if (local)
+    if (local())
     {
       // loop through all local nodes
       foreach_local_node(n, nodes)
@@ -343,23 +300,39 @@ int main(int argc, char** argv)
         // get xyz coordinates of a node
         node_xyz_fr_n(n, p4est, nodes, xyz);
 
-        // find coordinates of the point where interpolation is needed
-        xyz[0] += displace*nx*diag_min; xyz[0] = MAX(xyz[0], xyz_min[0]); xyz[0] = MIN(xyz[0], xyz_max[0]);
-        xyz[1] += displace*ny*diag_min; xyz[1] = MAX(xyz[1], xyz_min[1]); xyz[1] = MIN(xyz[1], xyz_max[1]);
+        xyz_fv[0] -= .5*dxyz[0];
+        xyz_fv[1] -= .5*dxyz[1];
 #ifdef P4_TO_P8
-        xyz[2] += displace*nz*diag_min; xyz[2] = MAX(xyz[2], xyz_min[2]); xyz[2] = MIN(xyz[2], xyz_max[2]);
+        xyz_fv[2] -= .5*dxyz[2];
 #endif
 
-        for (int j = 0; j < num_points; ++j)
-          for (int i = 0; i < num_points; ++i)
-          {
+        double max_error_local = 0;
+
+#ifdef P4_TO_P8
+        for (int k = 0; k < num_points(); ++k)
+#endif
+          for (int j = 0; j < num_points(); ++j)
+            for (int i = 0; i < num_points(); ++i)
+            {
+              xyz_fv[0] = xyz[0]+double(i)*dxyz_fv[0]; xyz_fv[0] = MAX(xyz_fv[0], xyz_min[0]); xyz_fv[0] = MIN(xyz_fv[0], xyz_max[0]);
+              xyz_fv[1] = xyz[1]+double(j)*dxyz_fv[1]; xyz_fv[1] = MAX(xyz_fv[1], xyz_min[1]); xyz_fv[1] = MIN(xyz_fv[1], xyz_max[1]);
+#ifdef P4_TO_P8
+              xyz_fv[2] = xyz[2]+double(k)*dxyz_fv[2]; xyz_fv[2] = MAX(xyz_fv[2], xyz_min[2]); xyz_fv[2] = MIN(xyz_fv[2], xyz_max[2]);
+#endif
+
             // interpolate
 #ifdef P4_TO_P8
             f_interp_ptr[n] = interp_l(xyz[0], xyz[1], xyz[2]);
+            f_exact_ptr [n] = f_cf(xyz[0], xyz[1], xyz[2]);
 #else
             f_interp_ptr[n] = interp_l(xyz[0], xyz[1]);
+            f_exact_ptr [n] = f_cf(xyz[0], xyz[1]);
 #endif
+            max_error_local = MAX(max_error_local, fabs(f_interp_ptr[n]-f_exact_ptr [n]));
           }
+
+        error_ptr[n] = max_error_local;
+        error_max_cur = MAX(error_max_cur, max_error_local);
       }
     }
     else
@@ -370,62 +343,43 @@ int main(int argc, char** argv)
         // get xyz coordinates of a node
         node_xyz_fr_n(n, p4est, nodes, xyz);
 
-        // find coordinates of the point where interpolation is needed
-        xyz[0] += displace*nx*diag_min; xyz[0] = MAX(xyz[0], xyz_min[0]); xyz[0] = MIN(xyz[0], xyz_max[0]);
-        xyz[1] += displace*ny*diag_min; xyz[1] = MAX(xyz[1], xyz_min[1]); xyz[1] = MIN(xyz[1], xyz_max[1]);
+        xyz_fv[0] -= .5*dxyz[0];
+        xyz_fv[1] -= .5*dxyz[1];
 #ifdef P4_TO_P8
-        xyz[2] += displace*nz*diag_min; xyz[2] = MAX(xyz[2], xyz_min[2]); xyz[2] = MIN(xyz[2], xyz_max[2]);
+        xyz_fv[2] -= .5*dxyz[2];
 #endif
 
-        for (int j = 0; j < num_points; ++j)
-          for (int i = 0; i < num_points; ++i)
-          {
+        double max_error_local = 0;
+
+#ifdef P4_TO_P8
+        for (int k = 0; k < num_points(); ++k)
+#endif
+          for (int j = 0; j < num_points(); ++j)
+            for (int i = 0; i < num_points(); ++i)
+            {
+              xyz_fv[0] = xyz[0]+double(i)*dxyz_fv[0]; xyz_fv[0] = MAX(xyz_fv[0], xyz_min[0]); xyz_fv[0] = MIN(xyz_fv[0], xyz_max[0]);
+              xyz_fv[1] = xyz[1]+double(j)*dxyz_fv[1]; xyz_fv[1] = MAX(xyz_fv[1], xyz_min[1]); xyz_fv[1] = MIN(xyz_fv[1], xyz_max[1]);
+#ifdef P4_TO_P8
+              xyz_fv[2] = xyz[2]+double(k)*dxyz_fv[2]; xyz_fv[2] = MAX(xyz_fv[2], xyz_min[2]); xyz_fv[2] = MIN(xyz_fv[2], xyz_max[2]);
+#endif
+
             // interpolate
 #ifdef P4_TO_P8
             f_interp_ptr[n] = interp_g(xyz[0], xyz[1], xyz[2]);
+            f_exact_ptr [n] = f_cf(xyz[0], xyz[1], xyz[2]);
 #else
             f_interp_ptr[n] = interp_g(xyz[0], xyz[1]);
+            f_exact_ptr [n] = f_cf(xyz[0], xyz[1]);
 #endif
+            max_error_local = MAX(max_error_local, fabs(f_interp_ptr[n]-f_exact_ptr [n]));
           }
+
+        error_ptr[n] = max_error_local;
+        error_max_cur = MAX(error_max_cur, max_error_local);
       }
     }
 
     timing.stop(); timing.read_duration();
-
-    // restore access to f_interp's data
-    ierr = VecRestoreArray(f_interp, &f_interp_ptr); CHKERRXX(ierr);
-
-    // ---------------------------------------------------------
-    // compute exact values and interpolation error
-    // ---------------------------------------------------------
-    // get access to Vec's data
-    ierr = VecGetArray(f_interp, &f_interp_ptr); CHKERRXX(ierr);
-    ierr = VecGetArray(f_exact,  &f_exact_ptr);  CHKERRXX(ierr);
-    ierr = VecGetArray(error,    &error_ptr);    CHKERRXX(ierr);
-
-    error_max_old = error_max_cur;
-    error_max_cur = 0;
-
-    // loop through local nodes
-    foreach_local_node(n, nodes)
-    {
-      // get xyz coordinates of a node
-      node_xyz_fr_n(n, p4est, nodes, xyz);
-
-      // find coordinates of the point where interpolation was performed
-      xyz[0] += displace*nx*diag_min; xyz[0] = MAX(xyz[0], xyz_min[0]); xyz[0] = MIN(xyz[0], xyz_max[0]);
-      xyz[1] += displace*ny*diag_min; xyz[1] = MAX(xyz[1], xyz_min[1]); xyz[1] = MIN(xyz[1], xyz_max[1]);
-#ifdef P4_TO_P8
-      xyz[2] += displace*nz*diag_min; xyz[2] = MAX(xyz[2], xyz_min[2]); xyz[2] = MIN(xyz[2], xyz_max[2]);
-#endif
-
-      // get exact value and compute error
-      f_exact_ptr[n] = f_cf.value(xyz);
-      error_ptr[n] = fabs(f_exact_ptr[n]-f_interp_ptr[n]);
-
-      // store max error
-      error_max_cur = MAX(error_max_cur, error_ptr[n]);
-    }
 
     // find max error among all processes
     int mpiret = MPI_Allreduce(MPI_IN_PLACE, &error_max_cur, 1, MPI_DOUBLE, MPI_MAX, mpi.comm()); SC_CHECK_MPI(mpiret);
@@ -436,7 +390,7 @@ int main(int argc, char** argv)
     ierr = VecRestoreArray(error,    &error_ptr);    CHKERRXX(ierr);
 
     // print out max error
-    ierr = PetscPrintf(mpi.comm(), "Grid levels: %2d / %2d, max interpolation error: %e, order: %1.3g\n", lmin+iter, lmax+iter, error_max_cur, log(error_max_old/error_max_cur)/log(2)); CHKERRXX(ierr);
+    ierr = PetscPrintf(mpi.comm(), "Grid levels: %2d / %2d, max interpolation error: %e, order: %1.3g\n", lmin()+iter, lmax()+iter, error_max_cur, log(error_max_old/error_max_cur)/log(2)); CHKERRXX(ierr);
 
     // ---------------------------------------------------------
     // save the grid and data into vtk
