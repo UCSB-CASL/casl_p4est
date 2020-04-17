98a1e4242263b9ca536c936005eb981a2d75e25e
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2020-04-17 15:48:45 -0700

    Code compiles with PetSc 3.12.  Finalized CMakeLists.txt.  Main 2d code is level-set reinitialization.

--- examples/fast_sweeping/main_2d.cpp
+++ examples/fast_sweeping/main_2d.cpp
@@ -1,105 +1,170 @@
-/*
- * Title: fast_sweeping
- * Description:
- * Author: Luis Ángel (임 영민)
- * Date Created: 04-16-2020
- */
+// System
+#include <stdexcept>
+#include <iostream>
 
-#ifndef P4_TO_P8
-#include <src/my_p4est_utils.h>
-#include <src/my_p4est_vtk.h>
-#include <src/my_p4est_nodes.h>
-#include <src/my_p4est_tools.h>
-#include <src/my_p4est_refine_coarsen.h>
-#include <src/my_p4est_log_wrappers.h>
-#include <src/my_p4est_node_neighbors.h>
-#include <src/my_p4est_macros.h>
-#else
+// casl_p4est
+#ifdef P4_TO_P8
 #include <src/my_p8est_utils.h>
 #include <src/my_p8est_vtk.h>
 #include <src/my_p8est_nodes.h>
 #include <src/my_p8est_tools.h>
 #include <src/my_p8est_refine_coarsen.h>
-#include <src/my_p8est_log_wrappers.h>
 #include <src/my_p8est_node_neighbors.h>
-#include <src/my_p8est_macros.h>
+#include <src/my_p8est_hierarchy.h>
+#include <src/my_p8est_level_set.h>
+#else
+#include <src/my_p4est_utils.h>
+#include <src/my_p4est_vtk.h>
+#include <src/my_p4est_nodes.h>
+#include <src/my_p4est_tools.h>
+#include <src/my_p4est_refine_coarsen.h>
+#include <src/my_p4est_node_neighbors.h>
+#include <src/my_p4est_hierarchy.h>
+#include <src/my_p4est_level_set.h>
 #endif
 
+#include <src/petsc_compatibility.h>
 #include <src/Parser.h>
 #include <src/casl_math.h>
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-
-	// prepare parallel enviroment
-	mpi_environment_t mpi;
-	mpi.init( argc, argv );
-
-	// stopwatch
-	parStopWatch w;
-	w.start( "Running example: fast_sweeping" );
-
-	// p4est variables
-	p4est_t *p4est;
-	p4est_nodes_t *nodes;
-	p4est_ghost_t *ghost;
-	p4est_connectivity_t *conn;
-	my_p4est_brick_t brick;
-
-	// domain size information
-	const int n_xyz[] = {1, 1, 1};
-	const double xyz_min[] = {-1, -1, -1};
-	const double xyz_max[] = {1, 1, 1};
-	const int periodic[] = {0, 0, 0};
-	conn = my_p4est_brick_new( n_xyz, xyz_min, xyz_max, &brick, periodic );
-
-	// create the forest
-	p4est = my_p4est_new( mpi.comm(), conn, 0, NULL, NULL );
-
-	// refine based on distance to a level-set
 #ifdef P4_TO_P8
-	struct:CF_3{
-	  double operator()(double x, double y, double z) const {
-		return 0.5 - sqrt(SQR(x) + SQR(y) + SQR(z));
-	  }
-	} circle;
+struct circle:CF_3{
+  circle(double x0_, double y0_, double z0_, double r_): x0(x0_), y0(y0_), z0(z0_), r(r_) {}
+  double operator()(double x, double y, double z) const {
+    return r - sqrt(SQR(x-x0) + SQR(y-y0) + SQR(z-z0));
+  }
+private:
+  double x0, y0, z0, r;
+};
 #else
-	struct : CF_2
+struct circle:CF_2
+{
+	circle( double x0_, double y0_, double r_ ) : x0( x0_ ), y0( y0_ ), r( r_ )
+	{}
+
+	double operator()( double x, double y ) const override
 	{
-		double operator()( double x, double y ) const
-		{
-			return 0.5 - sqrt( SQR( x ) + SQR( y ));
-		}
-	} circle;
+		return r - sqrt( SQR( x - x0 ) + SQR( y - y0 ));
+	}
+
+private:
+	double x0, y0, r;
+};
 #endif
 
-	splitting_criteria_cf_t sp( 3, 8, &circle );
-	p4est->user_pointer = &sp;
-	my_p4est_refine( p4est, P4EST_TRUE, refine_levelset_cf, NULL );
+int main (int argc, char* argv[])
+{
+	try
+	{
+		mpi_environment_t mpi{};
+		mpi.init( argc, argv );
 
-	// partition the forest
-	my_p4est_partition( p4est, P4EST_TRUE, NULL );
+		p4est_t *p4est;
+		p4est_nodes_t *nodes;
+		PetscErrorCode ierr;
 
-	// create ghost layer
-	ghost = my_p4est_ghost_new( p4est, P4EST_CONNECT_FULL );
+		cmdParser cmd;
+		cmd.add_option( "lmin", "min level for refinement" );
+		cmd.add_option( "lmax", "max level for refinement" );
+		cmd.parse( argc, argv );
 
-	// create node structure
-	nodes = my_p4est_nodes_new( p4est, ghost );
+#ifdef P4_TO_P8
+		circle circ(0, 0, 0, 0.3);
+#else
+		circle circ( 0, 0, 0.3 );
+#endif
+		splitting_criteria_cf_t cf_circle( cmd.get( "lmin", 3 ), cmd.get( "lmax", 6 ), &circ );
 
-	// save the grid into vtk
-	my_p4est_vtk_write_all( p4est, nodes, ghost,
-							P4EST_TRUE, P4EST_TRUE,
-							0, 0, "fast_sweeping" );
+		parStopWatch w;
+		w.start( "total time" );
 
-	// destroy the structures
-	p4est_nodes_destroy( nodes );
-	p4est_ghost_destroy( ghost );
-	p4est_destroy( p4est );
-	my_p4est_brick_destroy( conn, &brick );
+		/* Create the connectivity object */
+		p4est_connectivity_t *connectivity;
+		my_p4est_brick_t brick;
+		int n_xyz[] = {1, 1, 1};
+		double xyz_min[] = {-1, -1, -1};
+		double xyz_max[] = {1, 1, 1};
+		int periodic[] = {0, 0, 0};
 
-	w.stop();
-	w.read_duration();
+		connectivity = my_p4est_brick_new( n_xyz, xyz_min, xyz_max, &brick, periodic );
+
+		/* Now create the forest */
+		p4est = my_p4est_new( mpi.comm(), connectivity, 0, nullptr, nullptr );
+
+		/* refine the forest using a refinement criteria */
+		p4est->user_pointer = ( void * ) (&cf_circle);
+		my_p4est_refine( p4est, P4EST_TRUE, refine_levelset_cf, nullptr );
+
+		/* Finally re-partition */
+		my_p4est_partition( p4est, P4EST_TRUE, nullptr );
+
+		/* Create the ghost structure */
+		p4est_ghost_t *ghost = my_p4est_ghost_new( p4est, P4EST_CONNECT_FULL );
+
+		/* generate the node data structure */
+		nodes = my_p4est_nodes_new( p4est, ghost );
+
+		/* initialize the neighbor nodes structure */
+		my_p4est_hierarchy_t hierarchy( p4est, ghost, &brick );
+		my_p4est_node_neighbors_t node_neighbors( &hierarchy, nodes );
+
+		Vec phi;
+		ierr = VecCreateGhostNodes( p4est, nodes, &phi );
+		CHKERRXX( ierr );
+
+		double *phi_p;
+		ierr = VecGetArray( phi, &phi_p );
+		CHKERRXX( ierr );
+		for( size_t i = 0; i < nodes->indep_nodes.elem_count; ++i )
+		{
+			double xyz[P4EST_DIM];
+			node_xyz_fr_n( i, p4est, nodes, xyz );
+
+#ifdef P4_TO_P8
+			phi_p[i] = circ(xyz[0], xyz[1], xyz[2]);
+#else
+			phi_p[i] = circ( xyz[0], xyz[1] );
+#endif
+		}
+
+		ierr = VecRestoreArray( phi, &phi_p );
+		CHKERRXX( ierr );
+
+		my_p4est_level_set_t ls( &node_neighbors );
+		ls.reinitialize_2nd_order( phi, 100 );
+
+		std::ostringstream oss;
+		oss << "phi_" << mpi.size() << "_" << P4EST_DIM;
+		ierr = VecGetArray( phi, &phi_p );
+		CHKERRXX( ierr );
+		my_p4est_vtk_write_all( p4est, nodes, ghost,
+								P4EST_TRUE, P4EST_TRUE,
+								1, 0, oss.str().c_str(),
+								VTK_POINT_DATA, "phi", phi_p );
+		my_p4est_vtk_write_ghost_layer( p4est, ghost );
+		ierr = VecRestoreArray( phi, &phi_p );
+		CHKERRXX( ierr );
+
+		/* finally, delete PETSc Vecs by calling 'VecDestroy' function */
+		ierr = VecDestroy( phi );
+		CHKERRXX( ierr );
+
+		/* destroy the p4est and its connectivity structure */
+		p4est_nodes_destroy( nodes );
+		p4est_ghost_destroy( ghost );
+		p4est_destroy( p4est );
+		p4est_connectivity_destroy( connectivity );
+
+		w.stop();
+		w.read_duration();
+	}
+	catch( const std::exception &e )
+	{
+		cerr << e.what() << endl;
+	}
+
+	return 0;
 }
 
