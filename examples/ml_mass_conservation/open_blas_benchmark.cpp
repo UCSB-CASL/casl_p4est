0dcbea7f51261be071a473b6cc6228f11f859fce
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2021-07-23 13:03:38 -0700

    Updated OpenBlas benchmark now reading weights from JSON configuration for neural network.  Results match almost exactly those obtained with Frugally Deep library.  18% of samples didn't match exactly, with differences oscillating between 1.192e-7 to 7.45e-9.

--- examples/ml_mass_conservation/open_blas_benchmark.cpp
+++ examples/ml_mass_conservation/open_blas_benchmark.cpp
@@ -6,7 +6,7 @@
  *
  * Author: Luis Ángel (임 영민)
  * Created: July 20, 2021.
- * Updated: July 21, 2021.
+ * Updated: July 23, 2021.
  */
 
 #ifdef _OPENMP
@@ -47,20 +47,6 @@ int main( int argc, char** argv )
 		nThreads += 1;
 
 		std::cout << "Rank " << mpi.rank() << " can spawn " << nThreads << " thread(s)\n\n";
-
-		////////////////////////////////// Testing Base64 params load from json file ///////////////////////////////////
-
-		using json = nlohmann::json;
-		std::ifstream in( "/Users/youngmin/nnets/mass_nnet.json" );
-		json test;
-		in >> test;
-
-		int inputShape = test["input_shape"][0].get<int>();
-		auto nHiddenLayers = test["hidden_layers"].size();
-		auto hiddenShape = test["hidden_layers"][0]["shape"].get<std::vector<int>>();
-		auto hiddenWeights = fdeep::internal::decode_floats( test["hidden_layers"][0]["weights"] );
-
-		std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -121,6 +107,26 @@ int main( int argc, char** argv )
 
 		////////////////////////////////////////// Performance using OpenBlas //////////////////////////////////////////
 
+		// Let's load the nnet params JSON file.
+		using json = nlohmann::json;
+		std::ifstream in( "/Users/youngmin/nnets/mass_nnet.json" );
+		json nnet;
+		in >> nnet;
+
+		assert( N_FEATURES == nnet["input_shape"][0].get<int>() );	// Verify number of features corresponds to input size.
+		const auto N_LAYERS = nnet["hidden_layers"].size() + 1;		// Count the output layer too.
+		int sizes[N_LAYERS][2];		// Matrix sizes (m, k).  m = layer size, k = input size, (n = number of samples).
+		for( int i = 0; i < N_LAYERS - 1; i++ )
+		{
+			auto shape = nnet["hidden_layers"][i]["shape"].get<std::vector<int>>();
+			sizes[i][0] = shape[0];	// m.
+			sizes[i][1] = shape[1];	// k.
+		}
+		auto shape = nnet["output"]["shape"].get<std::vector<int>>();
+		assert( shape[0] == 1 );	// Expecting a single output neuron.
+		sizes[N_LAYERS - 1][0] = shape[0];	// 1.
+		sizes[N_LAYERS - 1][1] = shape[1];	// k.
+
 		// Adding bias entry to inputs1 and rearrange so that each column is a sample (rather than a row).
 		FDEEP_FLOAT_TYPE inputs1b[(N_FEATURES + 1) * N_SAMPLES];
 		for( int j = 0; j < N_FEATURES; j++ )
@@ -131,31 +137,24 @@ int main( int argc, char** argv )
 		for( int i = 0; i < N_SAMPLES; i++ )
 			inputs1b[N_FEATURES * N_SAMPLES + i] = 1;
 
-		// Loading weights: just random values.
-		const int LAYER_SIZE = 130;							// Hidden plus output layers.
+		// Loading weights.
 		std::vector<std::vector<FDEEP_FLOAT_TYPE>> W;
-		const int N_LAYERS = 5;
-		int sizes[][3] = {
-			{LAYER_SIZE, N_FEATURES + 1, N_SAMPLES},		// W0 x I0.
-			{LAYER_SIZE, LAYER_SIZE + 1, N_SAMPLES},		// W1 x f(W0 x I0).
-			{LAYER_SIZE, LAYER_SIZE + 1, N_SAMPLES},		// W2 x f(W1 x f(W0 x I0)).
-			{LAYER_SIZE, LAYER_SIZE + 1, N_SAMPLES},		// W3 x f(W2 x f(W1 x f(W0 x I0))).
-			{         1, LAYER_SIZE + 1, N_SAMPLES},		// W4 x f(W3 x f(W2 x f(W1 x f(W0 x I0)))).
-		};
 
 		for( int i = 0; i < N_LAYERS; i++ )
 		{
 			const int N_WEIGHTS = sizes[i][0] * sizes[i][1];
 			W.emplace_back( N_WEIGHTS );
-			for( auto& w : W[i] )
-				w = rand() / FDEEP_FLOAT_TYPE( RAND_MAX ) * (rand() / FDEEP_FLOAT_TYPE( RAND_MAX ) > 0.5? -1 : 1);	// NOLINT.
-		}							// When this loop ends, we have weights and bias all in row-majored weight matrices.
+			auto layer = (i < N_LAYERS - 1? nnet["hidden_layers"][i] : nnet["output"] );
+			auto weights = fdeep::internal::decode_floats( layer["weights"] );
+			for( int j = 0; j < N_WEIGHTS; j++ )
+				W[i][j] = weights[j];
+		}	// When this loop ends, we have weights and bias all in row-major (hidden and output) weight matrices.
 
 		// Allocating outputs.
 		std::vector<std::vector<FDEEP_FLOAT_TYPE>> O;
 		for( int i = 0; i < N_LAYERS; i++ )
 		{
-			const int N_OUTPUTS = (sizes[i][0] + (i == N_LAYERS - 1? 0 : 1)) * sizes[i][2];
+			const int N_OUTPUTS = (sizes[i][0] + (i == N_LAYERS - 1? 0 : 1)) * N_SAMPLES;
 			O.emplace_back( N_OUTPUTS, 1 );		// Adding the one for the bias too.
 		}
 
@@ -175,20 +174,33 @@ int main( int argc, char** argv )
 				const FDEEP_FLOAT_TYPE *input = inputs1b;
 				if( i > 0 )
 					input = O[i - 1].data();
-				cblas_sgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans, sizes[i][0], sizes[i][2], sizes[i][1], 1,
-							 W[i].data(), sizes[i][1], input, sizes[i][2], 0, O[i].data(), sizes[i][2] );
+				cblas_sgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans, sizes[i][0], N_SAMPLES, sizes[i][1], 1,
+							 W[i].data(), sizes[i][1], input, N_SAMPLES, 0, O[i].data(), N_SAMPLES );
 
 				// Apply ReLU activation function to all hidden layers.
 				if( i < N_LAYERS - 1 )
 				{
-					for( int j = 0; j < sizes[i][0] * sizes[i][2]; j++ )    // Activation function doesn't affect the bias.
+					for( int j = 0; j < sizes[i][0] * N_SAMPLES; j++ )    // Activation function doesn't affect the bias.
 						O[i][j] = MAX( 0.0f, O[i][j] );
 				}
 			}
 
 			// Add inputs2 to error-correcting output.
 			for( int i = 0; i < N_SAMPLES; i++ )
-				outputs[i] = O[N_LAYERS - 1][i] + FDEEP_FLOAT_TYPE( inputs2[i] );
+			{
+				auto input2 = (FDEEP_FLOAT_TYPE)inputs2[i];
+				if( iter == 0 )
+				{
+					FDEEP_FLOAT_TYPE diff = outputs[i] - (O[N_LAYERS - 1][i] + input2);
+					if( diff != 0 )
+					{
+						printf( "   Difference of %+1.8g for sample %i (%1.8g vs. %1.8g)\n",
+								diff, i, outputs[i], O[N_LAYERS - 1][i] + input2 );
+					}
+				}
+
+				outputs[i] = O[N_LAYERS - 1][i] + input2;
+			}
 
 			if( iter != 0 )
 			{
