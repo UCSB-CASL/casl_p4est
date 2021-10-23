1641b456d9b595d57c86f2229878eb468cba17af
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2021-10-23 14:13:52 -0700

    First commit for new approach to computing curvature for various levels of resolution. Added code to explore curvature spectrum with different star shapes.

--- examples/ml_curvature/local_utils.h
+++ examples/ml_curvature/local_utils.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <src/my_p4est_utils.h>
+#include "data_sets/star_theta_root_finding.h"
 
 namespace kutils
 {
@@ -161,6 +162,146 @@ namespace kutils
 	void rotateStencilToFirstQuadrant( std::vector<double>& stencil, const double gradient[P4EST_DIM] )
 	{
 		rotateStencilToFirstQuadrant( stencil.data(), gradient );
+	}
+
+	/**
+	 * Generate the sample row of level-set function values and target hk for a node next to the star interface.
+	 * @param [in] nodeIdx Query node adjancent or on the interface.
+	 * @param [in] NUM_COLUMNS Number of columns in output file.
+	 * @param [in] H Spacing (smallest quad/oct side-length).
+	 * @param [in] stencil The full uniform stencil of indices centered at the query node.
+	 * @param [in] p4est Pointer to p4est data structure.
+	 * @param [in] nodes Pointer to nodes data structure.
+	 * @param [in] phiReadPtr Pointer to level-set function values, backed by a parallel PETSc ghosted vector.
+	 * @param [in] star The level-set function with a star-shaped interface.
+	 * @param [in] gen Random-number generator device.
+	 * @param [in] normalDistribution A normal random variable distribution.
+	 * @param [in/out] pointsFile Pointer to optional file object to write coordinates of nodes adjacent to Gamma.
+	 * @param [in/out] anglesFile Pointer to optional file object to write angles of normal projected points on Gamma.
+	 * @param [out] distances A vector of "true" distances from all of 9 stencil points to the star-shaped level-set.
+	 * @param [out] pOnGamma Normal projection onto interface.
+	 * @param [in,out] visitedNodes Hash map functioning as a memoization mechanism to speed up access to visited nodes.
+	 * @param [in] normalReadPtr Pointer to normal vector components backed by parallel PETSc vectors.
+	 * @return Vector of sampled, reinitialized level-set function values for the stencil centered at the nodeIdx node.
+	 */
+	std::vector<double> sampleNodeNextToStarInterface( const p4est_locidx_t nodeIdx, const int NUM_COLUMNS,
+													   const double H, const std::vector<p4est_locidx_t>& stencil,
+													   const p4est_t *p4est, const p4est_nodes_t *nodes,
+													   const double *phiReadPtr, const geom::Star& star,
+													   std::mt19937& gen, std::normal_distribution<double>& normalDistribution,
+													   std::ofstream *pointsFile, std::ofstream *anglesFile,
+													   std::vector<double>& distances, double pOnGamma[P4EST_DIM],
+													   std::unordered_map<p4est_locidx_t, Point2>& visitedNodes,
+													   const double *normalReadPtr[P4EST_DIM] )
+	{
+		std::vector<double> sample( NUM_COLUMNS, 0 );		// Level-set function values and target hk.
+		distances.clear();
+		distances.reserve( NUM_COLUMNS );					// True distances and target hk.
+
+		int s;												// Index to fill in the sample vector.
+		double xyz[P4EST_DIM];
+		double pOnInterface[P4EST_DIM];
+		double theta, r, valOfDerivative, centerTheta;
+		double dx, dy, newDistance;
+		for( s = 0; s < num_neighbors_cube; s++ )			// Collect phi(x) for each of the 9 grid points.
+		{
+			sample[s] = phiReadPtr[stencil[s]];				// This is the distance obtained after reinitialization.
+
+			// Approximate position of point projected on interface.
+			const double grad[P4EST_DIM] = {DIM( normalReadPtr[0][stencil[s]], normalReadPtr[1][stencil[s]], normalReadPtr[2][stencil[s]] )};
+			node_xyz_fr_n( stencil[s], p4est, nodes, xyz );
+			for( int dim = 0; dim < P4EST_DIM; dim++ )
+				pOnInterface[dim] = xyz[dim] - grad[dim] * sample[s];
+
+			if( s == 4 )	// Rough estimation of point on interface, where curvature will be interpolated.
+			{
+				for( int dim = 0; dim < P4EST_DIM; dim++ )
+					pOnGamma[dim] = pOnInterface[dim];
+			}
+
+			// Get initial angle for polar approximation to point on star interface.
+			theta = atan2( pOnInterface[1], pOnInterface[0] );
+			theta = ( theta < 0 )? theta + 2 * M_PI : theta;
+			r = star.r( theta );
+			pOnInterface[0] = r * cos( theta );
+			pOnInterface[1] = r * sin( theta );				// Better approximation of projection of stencil point onto star.
+
+//		if( s == 4 )
+//		{
+//			std::cout << std::setprecision( 15 )
+//					  << "plot(" << xyz[0] << ", " << xyz[1] << ", 'b.', " << pOnInterfaceX << ", " << pOnInterfaceY
+//					  << ", 'mo');" << std::endl;
+//		}
+
+			// Compute current distance to Gamma using the improved point on interface.
+			dx = xyz[0] - pOnInterface[0];
+			dy = xyz[1] - pOnInterface[1];
+			distances.push_back( sqrt( SQR( dx ) + SQR( dy ) ) );
+
+			// Find theta that yields "a" minimum distance between stencil point and star using Newton-Raphson's method.
+			if( distances.back() > EPS )
+			{
+				if( visitedNodes.find( stencil[s] ) != visitedNodes.end() )		// Speed up queries.
+				{
+					theta = visitedNodes[stencil[s]].x;			// First component is the angular parameter.
+					newDistance = visitedNodes[stencil[s]].y;	// Second component is the distance to Gamma.
+				}
+				else
+				{
+					valOfDerivative = 1;
+					theta = distThetaDerivative( stencil[s], xyz[0], xyz[1], star, theta, H, gen, normalDistribution,
+												 valOfDerivative, newDistance );
+
+//				if( s == 4 )
+//				{
+//					r = star.r( theta );					// Recalculating closest point on interface.
+//					xOnGamma = r * cos( theta );
+//					yOnGamma = r * sin( theta );
+//					std::cout << std::setprecision( 15 )
+//							  << "plot(" << xOnGamma << ", " << yOnGamma << ", 'ko');" << std::endl;
+//				}
+
+					double relDist = (newDistance - distances[s]) / distances[s];
+					if( relDist > 1e-8  )					// Verify that new point is closer than previous approximation.
+					{
+						std::ostringstream stream;
+						stream << "Failure with node " << stencil[s] << ".  Val. of Der: " << std::scientific << valOfDerivative
+							   << std::scientific << std::setprecision( 15 ) << ".  New dist: " << newDistance
+							   << ".  Old dist: " << distances[s]
+							   << ".  Rel dist: " << relDist;
+						throw std::runtime_error( stream.str() );
+					}
+
+					visitedNodes[stencil[s]] = Point2( theta, newDistance );		// Memorize information for visited node.
+				}
+
+				distances[s] = newDistance;					// Root finding was successful: keep minimum distance.
+			}
+
+			if( star( xyz[0], xyz[1] ) < 0 )				// Fix sign.
+				distances[s] *= -1;
+
+			if( s == 4 )									// For center node we need theta to yield curvature.
+				centerTheta = theta;
+		}
+
+		sample[s] = H * star.curvature( centerTheta );		// Last column holds h*kappa.
+		distances.push_back( sample[s] );
+
+		// Write center sample node index and coordinates.
+		if( pointsFile )
+		{
+			node_xyz_fr_n( nodeIdx, p4est, nodes, xyz );
+			*pointsFile << nodeIdx << "," << xyz[0] << "," << xyz[1] << std::endl;
+		}
+
+		// Write angle parameter for projected point on interface.
+		if( anglesFile )
+		{
+			*anglesFile << ( centerTheta < 0 ? 2 * M_PI + centerTheta : centerTheta ) << std::endl;
+		}
+
+		return sample;
 	}
 }
 
