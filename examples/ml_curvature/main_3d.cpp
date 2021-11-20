fe03e0a136edfd420824b0ce1ce145500ac5ece2
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2021-11-19 17:02:19 -0800

    Fixed errors that prevent compilation of a barely simple C++ program in 3D, especially with the semi-Lagrangian ML class and nodes along interface.

--- examples/ml_curvature/main_3d.cpp
+++ examples/ml_curvature/main_3d.cpp
@@ -1,3 +1,9 @@
 #include <src/my_p4est_to_p8est.h>
-#include "main_2d.cpp"
+#include <iostream>
+//#include "main_2d.cpp"
 
+int main()
+{
+	std::cout << "Test in 3D" << std::endl;
+	return 0;
+}
--- src/my_p8est_semi_lagrangian_ml.h
+++ src/my_p8est_semi_lagrangian_ml.h
@@ -6,7 +6,7 @@
 #define ML_MASS_CONSERVATION_MY_P8EST_SEMI_LAGRANGIAN_ML_H
 
 #include <src/my_p4est_to_p8est.h>			// Define P4_TO_P8 so that we can compile my_p4est_semi_lagrangian_ml.cpp
-#include "my_p4est_semi_lagrangian_ml.h"	// with that macro on.
+// #include "my_p4est_semi_lagrangian_ml.h"	// with that macro on.
 
 
 #endif //ML_MASS_CONSERVATION_MY_P8EST_SEMI_LAGRANGIAN_ML_H
