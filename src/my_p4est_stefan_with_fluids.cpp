aad1d976800f7d9ad444c3a82e619969b2fa4fe8
Baptiste <baptistejouron@ucsb.edu>
2022-05-17 15:52:55 -0700

    Fixed an issue with save/load in the NS only case

--- src/my_p4est_stefan_with_fluids.cpp
+++ src/my_p4est_stefan_with_fluids.cpp
@@ -520,17 +520,21 @@ void my_p4est_stefan_with_fluids_t::initialize_grids_and_fields_from_load_state(
   if(hierarchy_n!=NULL) {
     delete hierarchy_n;
   }
+
   if(ngbd_n!=NULL) {delete ngbd_n;}
+
 
   hierarchy_n = new my_p4est_hierarchy_t(p4est_n, ghost_n, &brick);
   ngbd_n = new my_p4est_node_neighbors_t(hierarchy_n, nodes_n);
   ngbd_n->init_neighbors();
+
 
   // Update the neigborhood and hierarchy:
   if(hierarchy_np1!=NULL) {
     delete hierarchy_np1;
   }
   if(ngbd_np1!=NULL) {delete ngbd_np1;}
+
 
   hierarchy_np1 = new my_p4est_hierarchy_t(p4est_np1, ghost_np1, &brick);
   ngbd_np1 = new my_p4est_node_neighbors_t(hierarchy_np1, nodes_np1);
@@ -546,17 +550,20 @@ void my_p4est_stefan_with_fluids_t::initialize_grids_and_fields_from_load_state(
 
   // Extend fields:
   // TO-DO : if defn of extension bands is generalized, make sure that is applied here (or maybe we should just make a function called "compute extension bands" and use that everywhere
-  dxyz_min(p4est_np1, dxyz_smallest);
-  min_volume_ = MULTD(dxyz_smallest[0], dxyz_smallest[1], dxyz_smallest[2]);
-  extension_band_use_    = (8.)*pow(min_volume_, 1./ double(P4EST_DIM)); //8
-  extension_band_extend_ = 10.*pow(min_volume_, 1./ double(P4EST_DIM)); //10
-  dxyz_close_to_interface = dxyz_close_to_interface_mult*MAX(dxyz_smallest[0],dxyz_smallest[1]);
+  if(solve_stefan)
+  {
+      dxyz_min(p4est_np1, dxyz_smallest);
+      min_volume_ = MULTD(dxyz_smallest[0], dxyz_smallest[1], dxyz_smallest[2]);
+      extension_band_use_    = (8.)*pow(min_volume_, 1./ double(P4EST_DIM)); //8
+      extension_band_extend_ = 10.*pow(min_volume_, 1./ double(P4EST_DIM)); //10
+      dxyz_close_to_interface = dxyz_close_to_interface_mult*MAX(dxyz_smallest[0],dxyz_smallest[1]);
 
-  extend_relevant_fields();
+      extend_relevant_fields();
 
-  // Compute vinterface:
-  v_interface.create(p4est_np1, nodes_np1);
-  compute_interfacial_velocity();
+      // Compute vinterface:
+      v_interface.create(p4est_np1, nodes_np1);
+      compute_interfacial_velocity();
+  }
 
   load_tstep =tstep;
   tstart=tn;
